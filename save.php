d937cd70a26a5bd4992d938e2910d6852c0f1d10
tradoncic1 <tarik.r65@gmail.com>
2019-01-13 15:58:48 +0100

    Comment changes

--- save.php
+++ save.php
@@ -1,9 +1,11 @@
+<!--this file serves to edit the followers.txt file used to keep old follower count-->
 <?php
 	$myFile = "followers.txt";
 
+	//opens the file
 	$fh = fopen($myFile, 'w') or die("can't open file");
+	//saves the post value in a variable
 	$stringData = $_POST["name"];
-	echo "<script type='text/javascript'>alert('$stringData');</script>";
 	fwrite($fh, $stringData);
 	fclose($fh);
-?>  
+?>
